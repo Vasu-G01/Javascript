9438a22b2b5aa0e5297a2e9665eb818f40044f0e
Vasu-G01 <vasugumber463@gmail.com>
2025-05-11 12:30:05 +0530

    Add files via upload

---
+++ 01_var.js
@@ -0,0 +1,16 @@
+const accountID=123336
+let accountEmail="Vasu@gmail.com"
+var accountPass="34567"
+accountCity="Fazilka"
+let accountState;
+accountEmail="hc@hc.com"
+accountPass="234322"
+accountCity="Bathinda"
+accountState="Punjab"
+// accountID=22   //We Cant change value of const once it declared
+console.log(accountID)
+console.table([accountID,accountEmail,accountPass,accountCity,accountState])
+/*
+Multi
+line
+comment*/
---
+++ 02_datatype.js
@@ -0,0 +1,17 @@
+"use strict" // treat all js as newer version of js
+// alert("Hello World") Alert has different syntax in text editor so we cant use.
+console.log(3
+    +3
+) //Syntax is correct but it creates difficulty to read Focus on Readability of code.
+let name="Vasu" //String
+let age=18 //Number
+let logIn=true //Boolean
+let state;
+
+console.log(typeof undefined)
+console.log(typeof null) //Imp interview ques
+let a = 10
+let b = 5
+console.log(a-b)
+console.log(a+b)
+console.log(a*b)
